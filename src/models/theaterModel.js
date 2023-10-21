db67b7d955159716445abaf0b24fb269823334ee
Mohamed Akmal <muhammedakmal43@gmail.com.>
2023-10-21 16:47:39 +0530

    added theater model

---
+++ src/models/theaterModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const theaterModel = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  isDelete: {
+    type: Boolean,
+    default: false,
+    require: true,
+  },
+  isApprove: {
+    type: Boolean,
+    default: false,
+    require: true,
+  },
+});
+
+const theaterSchema = new mongoose.model("Theater", theaterModel);
+module.exports = theaterSchema;
